2d9818149c47bd2fbc659422e4a9dba1e9770f8e
Emilio Pereira <91755231+Emiii12@users.noreply.github.com>
2025-03-27 22:04:52 -0300

    update: home section

--- my-portfolio/src/components/HomeSection/subcomponents/ProfileImage.tsx
+++ my-portfolio/src/components/HomeSection/subcomponents/ProfileImage.tsx
@@ -5,7 +5,7 @@ import ImgFoto from '../../../../public/image/fotocv.webp';
 const ProfileImage = () => {
   return (
     <div className='p-1 pb-3 rounded-full border-[2px] border-vibrantOrange '>
-      <div className='w-60 h-60 smx:w-64 smx:h-64 md:w-72 md:h-72 '>
+      <div className='w-60 h-60 smx:w-64 smx:h-64 md:w-80 md:h-80 '>
         <Image src={ImgFoto} alt='Foto de Emilio Pereira' className='rounded-full'/>
       </div>
     </div>
