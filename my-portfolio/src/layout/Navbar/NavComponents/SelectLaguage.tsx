68aee0ac3c4cce4dd8bd98c9ccc6cf75a0e13110
Emilio Pereira <91755231+Emiii12@users.noreply.github.com>
2025-04-22 14:01:41 -0300

    footer and header translated

--- my-portfolio/src/layout/Navbar/NavComponents/SelectLaguage.tsx
+++ my-portfolio/src/layout/Navbar/NavComponents/SelectLaguage.tsx
@@ -1,13 +1,25 @@
+'use client';
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
-import React, { useState } from 'react';
 import { CustomSelect } from '@/ui/input/CustomSelect';
+import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
+import { useState, useEffect } from 'react';
 
 export const SelectLanguage = () => {
+  const { i18n } = useTranslation();
   const [language, setLanguage] = useState('es');
 
   const handleLanguageChange = (value: string) => {
+    localStorage.setItem('language', value);
+    i18next.changeLanguage(value);
     setLanguage(value);
   };
+
+  useEffect(() => {
+    const savedLang = localStorage.getItem('language') || 'es';
+    i18n.changeLanguage(savedLang);
+    setLanguage(savedLang);
+  }, [i18n]);
 
   return (
     <CustomSelect
@@ -15,7 +27,7 @@ export const SelectLanguage = () => {
       onChange={handleLanguageChange}
       options={[
         { value: 'es', label: 'Español' },
-        { value: 'en', label: 'Inglés' },
+        { value: 'en', label: 'English' },
       ]}
       icon={<ChevronDownIcon className='absolute w-5 h-5 text-lightSeaGreen' />}
       selectClass='pr-10 text-sm xl:text-base pl-2'
@@ -23,12 +35,22 @@ export const SelectLanguage = () => {
   );
 };
 
+
 export const SelectLanguageSideBar = () => {
+  const { i18n } = useTranslation();
   const [language, setLanguage] = useState('es');
 
   const handleLanguageChange = (value: string) => {
+    localStorage.setItem('language', value);
+    i18next.changeLanguage(value);
     setLanguage(value);
   };
+
+  useEffect(() => {
+    const savedLang = localStorage.getItem('language') || 'es';
+    i18n.changeLanguage(savedLang);
+    setLanguage(savedLang);
+  }, [i18n]);
 
   return (
     <CustomSelect
@@ -36,7 +58,7 @@ export const SelectLanguageSideBar = () => {
       onChange={handleLanguageChange}
       options={[
         { value: 'es', label: 'Español' },
-        { value: 'en', label: 'Inglés' },
+        { value: 'en', label: 'English' },
       ]}
       icon={<ChevronDownIcon className='w-4 h-4 text-lightSeaGreen xs:w-5 xs:h-5 sm:w-6 sm:h-6' />}
       selectClass='text-base sm:text-xl xs:text-lg px-0 py-1 '
--- my-portfolio/src/util/Providers.tsx
+++ my-portfolio/src/util/Providers.tsx
@@ -1,13 +1,45 @@
 "use client";
 import { AppProvider } from "@/context/AppContext";
 import { SidebarProvider } from "@/context/SidebarContext";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+import layout_es from '@/util/json/translations/es/layout.json';
+import common_es from '@/util/json/translations/es/common.json';
+
+import layout_en from '@/util/json/translations/en/layout.json';
+import common_en from '@/util/json/translations/en/common.json';
+
+const getUserLanguage = () => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem('language') || 'es';
+  }
+  return 'es';
+};
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: getUserLanguage(),
+  resources: {
+    es: { 
+      layout: layout_es,
+      common: common_es
+    },
+    en: { 
+      layout: layout_en,
+      common: common_en
+    },
+  }
+});
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SidebarProvider>
-      <AppProvider >
-        {children}
-      </AppProvider>
-    </SidebarProvider>
+    <I18nextProvider i18n={i18next}>
+      <SidebarProvider>
+        <AppProvider>
+          {children}
+        </AppProvider>
+      </SidebarProvider>
+    </I18nextProvider>
   );
 };
